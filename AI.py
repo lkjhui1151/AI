3f87120270cde954b138d4d3173b2df9a7e0a3c6
root <lkjhui1151@gmail.com>
2020-04-05 12:51:01 +0700

    AI First

---
+++ AI.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import webbrowser
+import time
+from time import ctime
+
+r = sr.Recognizer()
+
+def record_audio(ask = False):
+    with sr.Microphone() as source:
+         if ask:
+            print(ask)
+         audio = r.listen(source)
+         voice_data = ''
+         try:
+             voice_data = r.recognize_google(audio)
+         except sr.UnknownValueError:
+             print("Sorry, I did not get that.")
+         except sr.RequestError:
+             print('Sorry, my speech service is down.')
+         return voice_data
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+       print('My name is Mirot')
+    if 'what time is it' in voice_data:
+       print(ctime())
+    if 'search' in voice_data:
+       search = record_audio('what do you want to search for?')
+       url = 'https://google.com/search?q=' + search
+       webbrowser.get().open(url)
+       print('Here is what I found for ' + search)
+    if 'find location' in voice_data:
+       location = record_audio('what is the location?')
+       url = 'https://google.nl/maps/place/' + location + '/&amp;'
+       webbrowser.get().open(url)
+       print('Here is the location of ' + location)
+    if 'exit' in voice_data:
+       exit()
+
+time.sleep(1)
+print('How can I help you?')
+while 1:
+      voice_data = record_audio()
+      respond(voice_data)
