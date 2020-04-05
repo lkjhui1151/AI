f7b5cf77a602af712e54eb38a42895f94f80a876
Admin <lkjhui1151@gmail.com>
2020-04-05 10:25:18 -0700

    New

--- AI.py
+++ AI.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 import webbrowser
 import time
+import playsound
+import os
+import random
+from gtts import gTTS
 from time import ctime
 
 r = sr.Recognizer()
@@ -8,37 +12,46 @@ r = sr.Recognizer()
 def record_audio(ask = False):
     with sr.Microphone() as source:
          if ask:
-            print(ask)
+            Mirot_speak(ask)
          audio = r.listen(source)
          voice_data = ''
          try:
              voice_data = r.recognize_google(audio)
          except sr.UnknownValueError:
-             print("Sorry, I did not get that.")
+             Mirot_speak("Sorry, I did not get that.")
          except sr.RequestError:
-             print('Sorry, my speech service is down.')
+             Mirot_speak('Sorry, my speech service is down.')
          return voice_data
+
+def Mirot_speak(audio_string):
+   tts = gTTS(text=audio_string,lang='en')
+   r = random.randint(1,10000000)
+   audio_file = 'audio-'+str(r)+'.mp3'
+   tts.save(audio_file)
+   playsound.playsound(audio_file)
+   print(audio_string)
+   os.remove(audio_file)
 
 def respond(voice_data):
     if 'what is your name' in voice_data:
-       print('My name is Mirot')
+       Mirot_speak('My name is Mirot')
     if 'what time is it' in voice_data:
-       print(ctime())
+       Mirot_speak(ctime())
     if 'search' in voice_data:
        search = record_audio('what do you want to search for?')
        url = 'https://google.com/search?q=' + search
        webbrowser.get().open(url)
-       print('Here is what I found for ' + search)
+       Mirot_speak('Here is what I found for ' + search)
     if 'find location' in voice_data:
        location = record_audio('what is the location?')
        url = 'https://google.nl/maps/place/' + location + '/&amp;'
        webbrowser.get().open(url)
-       print('Here is the location of ' + location)
+       Mirot_speak('Here is the location of ' + location)
     if 'exit' in voice_data:
        exit()
 
 time.sleep(1)
-print('How can I help you?')
+Mirot_speak('How can I help you?')
 while 1:
       voice_data = record_audio()
       respond(voice_data)
