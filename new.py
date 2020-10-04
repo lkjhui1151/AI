f95495e94c1936fb015b74adc9c5b0ba3cde048a
lkjhui1151 <58293089+lkjhui1151@users.noreply.github.com>
2020-10-04 20:55:05 +0700

    Create new.py

---
+++ new.py
@@ -0,0 +1,76 @@
+import speech_recognition as sr  # recognise speech
+import playsound  # to play an audio file
+from gtts import gTTS  # google text to speech
+import random
+from time import ctime  # get time details
+import webbrowser  # open browser
+import ssl
+import certifi
+import time
+import os  # to remove created audio files
+from PIL import Image
+import subprocess
+import pyttsx3
+import bs4 as bs
+import requests
+
+
+class Person:
+    name = 'Patch'
+
+    def setName(self, name):
+        self.name = name
+
+
+person_obj = Person()
+r = sr.Recognizer()
+
+
+def record_audio(ask=""):
+    with sr.Microphone() as source:
+        if ask:
+            engine_speak(ask)
+        audio = r.listen(source)
+        print('Done Listening')
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:  # error: recognizer does not understand
+            engine_speak('Sorry, I did not get that')
+        except sr.RequestError:
+            engine_speak('Sorry, my speech service is down')
+        print(person_obj.name + " : ", voice_data.lower())
+        return voice_data.lower()
+
+
+def engine_speak(audio_string):
+    audio_string = str(audio_string)
+    tts = gTTS(text=audio_string, lang='en')  # text to speech(voice)s
+    r = random.randint(1, 20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file)  # save as mp3
+    playsound.playsound(audio_file)  # play the audio file
+    os.remove(audio_file)  # remove audio file
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        engine_speak('my name is Myroth')
+    if 'what time is it' in voice_data:
+        engine_speak(ctime())
+    if 'thank you' in voice_data:
+        engine_speak('your welcome.')
+        exit()
+    if voice_data == 'listen' in voice_data:
+        engine_speak('what do you want to do')
+        time.sleep(1)
+        if 'open door' in voice_data:
+            engine_speak('open door')
+            exit()
+
+
+time.sleep(1)
+engine_speak('How can I help you?')
+while True:
+    voice_data = record_audio()
+    respond(voice_data)
